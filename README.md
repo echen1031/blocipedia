c476c26e38fcae67aa0fff9712c4e4b1f324a09e
Eric Chen <eric87chen@gmail.com>
2014-05-12 13:52:27 -0400

    first comit

---
+++ README.md
@@ -0,0 +1,1 @@
+Build an application that allows users to create public and private Markdown-based wikis. We'll call our application "Blocipedia", but you're free to name it anything you want.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blocipedia::Application.config.secret_key_base = 'fd562b6a8c4eed75f593dee3dc4965a77bce15ce8f51b74bd2c7ad931f00a701139e046451ca2eb03b7534ab8535f75687e7784a5ecb45ee270cc3c78a974208'
